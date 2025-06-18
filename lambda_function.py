90c4c7d899b637abaee696c707063bdfdfe24d36
RutvikMangukiya <rutvikmangukiya007@gmail.com>
2025-06-18 16:53:17 +0530

    feature/devops_June_18 branch added

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: Apache-2.0  , ## changes here!!
 import json
 import urllib.parse
 import boto3
